63f27b7c8668414a164b6222018add392b0ca1f1
Joaquin Esquilin <esquilin.joaquin@gmail.com>
2020-01-15 04:10:56 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -3,3 +3,14 @@ def print_business_card(person)
   puts "Name: #{person[0]}, Age: #{person[1]}, Occupation: #{person[2]}"
   puts "Contact: #{person[3]}"
 end
+
+ 
+print_business_card(bob)
+ 
+# => "Name: Bob, Age: 46, Occupation: Juggler"
+# => "Contact: 555-555-5555"
+ 
+print_business_card(stefani)
+ 
+# => "Name: Stefani, Age: 49, Occupation: Firefighter"
+# => "Contact: 555-111-1111"
